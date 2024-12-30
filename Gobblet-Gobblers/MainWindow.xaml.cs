74952655c008454f5a609eda7bb47b9a6bf80a57
tatara-kj <130702084+tatara-kj@users.noreply.github.com>
2024-12-30 13:06:52 +0100

    30.12

--- Gobblet-Gobblers/MainWindow.xaml.cs
+++ Gobblet-Gobblers/MainWindow.xaml.cs
@@ -9,7 +9,13 @@ namespace Pozeracze
     public partial class MainWindow : Window
     {
         int rozmiarPlanszy = 3;
+        private int pionkiMalyGracz1 = 4;
+        private int pionkiSredniGracz1 = 4;
+        private int pionkiDuzyGracz1 = 4;
 
+        private int pionkiMalyGracz2 = 4;
+        private int pionkiSredniGracz2 = 4;
+        private int pionkiDuzyGracz2 = 4;
         public MainWindow()
         {
             InitializeComponent();
@@ -21,7 +27,7 @@ namespace Pozeracze
             menu.Visibility = Visibility.Collapsed;
             UstawNazwyGraczy();
 
-            //kolory obramowania graczy
+          
 
             UstawKolorButtona(gracz1_przycisk1, ColorComboBox);
             UstawKolorButtona(gracz1_przycisk2, ColorComboBox);
@@ -31,7 +37,10 @@ namespace Pozeracze
             UstawKolorButtona(gracz2_przycisk2, ColorComboBox_Kopiuj);
             UstawKolorButtona(gracz2_przycisk3, ColorComboBox_Kopiuj);
 
+           
 
+
+            AktualizujPionki();
         }
 
         private void GenerujPlansze()
@@ -39,10 +48,27 @@ namespace Pozeracze
             Plansza.Children.Clear();
             Plansza.RowDefinitions.Clear();
             Plansza.ColumnDefinitions.Clear();
+            rundaLabel.Content = "Runda gracza " + gracz1.Text;
 
             if (!int.TryParse(PoleRozmiar.Text, out rozmiarPlanszy) || rozmiarPlanszy < 3)
                 rozmiarPlanszy = 3;
 
+         
+            int liczbaPol = rozmiarPlanszy * rozmiarPlanszy;
+
+            
+            int pionkiNaPole = (liczbaPol / 6) * 6; 
+
+           
+            pionkiMalyGracz1 = (int)Math.Round(pionkiNaPole * 3.0 / 6);
+            pionkiSredniGracz1 = (int)Math.Round(pionkiNaPole * 2.0 / 6);
+            pionkiDuzyGracz1 = (int)Math.Round(pionkiNaPole * 1.0 / 6);
+
+            pionkiMalyGracz2 = (int)Math.Round(pionkiNaPole * 3.0 / 6);
+            pionkiSredniGracz2 = (int)Math.Round(pionkiNaPole * 2.0 / 6);
+            pionkiDuzyGracz2 = (int)Math.Round(pionkiNaPole * 1.0 / 6);
+
+            
             for (int i = 0; i < rozmiarPlanszy; i++)
             {
                 Plansza.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
@@ -65,14 +91,16 @@ namespace Pozeracze
                         Content = ""
                     };
 
-
-                    pole.MouseLeftButtonUp += PolePlanszy_Click;
+                    pole.MouseLeftButtonDown += PolePlanszy_Click;
 
                     Grid.SetRow(pole, wiersz);
                     Grid.SetColumn(pole, kolumna);
                     Plansza.Children.Add(pole);
                 }
             }
+
+          
+            AktualizujPionki();
         }
 
         private Brush kolorGracza1 = Brushes.Black;
@@ -152,122 +180,250 @@ namespace Pozeracze
 
         private void mieczGracz1_click(object sender, RoutedEventArgs e)
         {
-            czyWybrany = true;
-            gracz2_przycisk1.IsEnabled = false;
-            gracz2_przycisk2.IsEnabled = false;
-            gracz2_przycisk3.IsEnabled = false;
-
-            if (wybranyPrzyciskGracz1 != null)
+            if (czyRundaGracza1) 
             {
-                wybranyPrzyciskGracz1.Effect = null;
-            }
+                czyWybrany = true;
+                gracz2_przycisk1.IsEnabled = false;
+                gracz2_przycisk2.IsEnabled = false;
+                gracz2_przycisk3.IsEnabled = false;
 
-            var przycisk = sender as Button;
-            if (przycisk != null)
-            {
-                przycisk.Effect = new DropShadowEffect
+                if (wybranyPrzyciskGracz1 != null)
                 {
-                    Color = Colors.LightGoldenrodYellow,
-                    BlurRadius = 20,
-                    ShadowDepth = 5
-                };
+                    wybranyPrzyciskGracz1.Effect = null;
+                }
 
-                wybranyPrzyciskGracz1 = przycisk;
-                wybranyObraz = ((Image)przycisk.Content).Source;
+                var przycisk = sender as Button;
+                if (przycisk != null)
+                {
+                    przycisk.Effect = new DropShadowEffect
+                    {
+                        Color = Colors.LightGoldenrodYellow,
+                        BlurRadius = 20,
+                        ShadowDepth = 5
+                    };
+
+                    wybranyPrzyciskGracz1 = przycisk;
+                    wybranyObraz = ((Image)przycisk.Content).Source;
+                }
+
+                rundaLabel.Content = "Runda gracza " + gracz1.Text;
             }
-
-            rundaLabel.Content = "Runda gracza " + gracz1.Text;
+            else
+            {
+                MessageBox.Show("To nie Twoja kolej, poczekaj na swoją rundę!");
+            }
         }
 
+        private void AktualizujPionki()
+        {
+          
+            pionki_maly_gracz1.Content = $"x {pionkiMalyGracz1}";
+            pionki_sredni_gracz1.Content = $"x {pionkiSredniGracz1}";
+            pionki_duzy_gracz1.Content = $"x {pionkiDuzyGracz1}";
 
+            pionki_maly_gracz2.Content = $"x {pionkiMalyGracz2}";
+            pionki_sredni_gracz2.Content = $"x {pionkiSredniGracz2}";
+            pionki_duzy_gracz2.Content = $"x {pionkiDuzyGracz2}";
+        }
+
+        private void PostawPionek(string rodzajPionka, int gracz)
+        {
+            if (gracz == 1)
+            {
+                if (rodzajPionka == "Maly" && pionkiMalyGracz1 > 0)
+                {
+                    pionkiDuzyGracz1--;
+                    if (pionkiMalyGracz1 == 0)
+                    {
+                        gracz1_przycisk1.IsEnabled = false; 
+                    }
+                }
+                else if (rodzajPionka == "Sredni" && pionkiSredniGracz1 > 0)
+                {
+                    pionkiSredniGracz1--;
+                    if (pionkiSredniGracz1 == 0)
+                    {
+                        gracz1_przycisk2.IsEnabled = false; 
+                    }
+                }
+                else if (rodzajPionka == "Duzy" && pionkiDuzyGracz1 > 0)
+                {
+                    pionkiMalyGracz1--;
+                    if (pionkiDuzyGracz1 == 0)
+                    {
+                        gracz1_przycisk3.IsEnabled = false; 
+                    }
+                }
+            }
+            else if (gracz == 2)
+            {
+                if (rodzajPionka == "Maly" && pionkiMalyGracz2 > 0)
+                {
+                    pionkiMalyGracz2--;
+                    if (pionkiMalyGracz2 == 0)
+                    {
+                        gracz2_przycisk1.IsEnabled = false;
+                    }
+                }
+                else if (rodzajPionka == "Sredni" && pionkiSredniGracz2 > 0)
+                {
+                    pionkiSredniGracz2--;
+                    if (pionkiSredniGracz2 == 0)
+                    {
+                        gracz2_przycisk2.IsEnabled = false;
+                    }
+                }
+                else if (rodzajPionka == "Duzy" && pionkiDuzyGracz2 > 0)
+                {
+                    pionkiDuzyGracz2--;
+                    if (pionkiDuzyGracz2 == 0)
+                    {
+                        gracz2_przycisk3.IsEnabled = false;
+                    }
+                }
+            }
+
+            AktualizujPionki();
+        }
 
         private void mieczGracz2_click(object sender, RoutedEventArgs e)
         {
-            czyWybrany = true;
-           
-
-            if (wybranyPrzyciskGracz1 != null)
+            if (!czyRundaGracza1) 
             {
-                wybranyPrzyciskGracz1.Effect = null;
-            }
+                czyWybrany = true;
+                gracz1_przycisk1.IsEnabled = false;
+                gracz1_przycisk2.IsEnabled = false;
+                gracz1_przycisk3.IsEnabled = false;
 
-            var przycisk = sender as Button;
-            if (przycisk != null)
-            {
-                przycisk.Effect = new DropShadowEffect
+                if (wybranyPrzyciskGracz1 != null)
                 {
-                    Color = Colors.LightGoldenrodYellow,
-                    BlurRadius = 20,
-                    ShadowDepth = 5
-                };
+                    wybranyPrzyciskGracz1.Effect = null;
+                }
 
-                wybranyPrzyciskGracz1 = przycisk;
-                wybranyObraz = ((Image)przycisk.Content).Source;
+                var przycisk = sender as Button;
+                if (przycisk != null)
+                {
+                    przycisk.Effect = new DropShadowEffect
+                    {
+                        Color = Colors.LightGoldenrodYellow,
+                        BlurRadius = 20,
+                        ShadowDepth = 5
+                    };
+
+                    wybranyPrzyciskGracz1 = przycisk;
+                    wybranyObraz = ((Image)przycisk.Content).Source;
+                }
+
+                rundaLabel.Content = "Runda gracza " + gracz1_Kopiuj1.Text;
             }
-
-            rundaLabel.Content = "Runda gracza " + gracz1.Text;
+            else
+            {
+                MessageBox.Show("To nie Twoja kolej, poczekaj na swoją rundę!");
+            }
         }
-
+  
         private void PolePlanszy_Click(object sender, RoutedEventArgs e)
         {
             if (czyWybrany && wybranyObraz != null)
             {
-                
                 Label pole = sender as Label;
-                
+                pole.Content = new Image { Source = wybranyObraz, Stretch = Stretch.Uniform };
+
+                if (czyRundaGracza1)
+                {
+                    pole.Background = kolorGracza1;
+                    if (wybranyPrzyciskGracz1 == gracz1_przycisk1)
+                        PostawPionek("Maly", 1);
+                    else if (wybranyPrzyciskGracz1 == gracz1_przycisk2)
+                        PostawPionek("Sredni", 1);
+                    else if (wybranyPrzyciskGracz1 == gracz1_przycisk3)
+                        PostawPionek("Duzy", 1);
                    
-                    pole.Content = new Image { Source = wybranyObraz, Stretch = Stretch.Uniform };
+                }
+                else
+                {
+                    pole.Background = kolorGracza2;
+                    if (wybranyPrzyciskGracz1 == gracz2_przycisk1)
+                        PostawPionek("Maly", 2);
+                    else if (wybranyPrzyciskGracz1 == gracz2_przycisk2)
+                        PostawPionek("Sredni", 2);
+                    else if (wybranyPrzyciskGracz1 == gracz2_przycisk3)
+                        PostawPionek("Duzy", 2);
+                }
 
-                   
-                    if (czyRundaGracza1)
-                    {
-                        pole.Background = kolorGracza1;
-                    }
-                    else
-                    {
-                        pole.Background = kolorGracza2;
-                    }
+                czyWybrany = false;
+                wybranyObraz = null;
 
-                   
-                    czyWybrany = false;
-                    wybranyObraz = null;
+                czyRundaGracza1 = !czyRundaGracza1;
+                 UstawStanRundy();
 
-                 
-                    czyRundaGracza1 = !czyRundaGracza1;
-
-                    if (czyRundaGracza1)
-                    {
-                    
-                        gracz1_przycisk1.IsEnabled = true;
-                        gracz1_przycisk2.IsEnabled = true;
-                        gracz1_przycisk3.IsEnabled = true;
-
-                        gracz2_przycisk1.IsEnabled = false;
-                        gracz2_przycisk2.IsEnabled = false;
-                        gracz2_przycisk3.IsEnabled = false;
-
-                        rundaLabel.Content = "Runda gracza " + gracz1.Text;
-                    }
-                    else
-                    {
-                        
-                        gracz1_przycisk1.IsEnabled = false;
-                        gracz1_przycisk2.IsEnabled = false;
-                        gracz1_przycisk3.IsEnabled = false;
-
-                        gracz2_przycisk1.IsEnabled = true;
-                        gracz2_przycisk2.IsEnabled = true;
-                        gracz2_przycisk3.IsEnabled = true;
-
-                        rundaLabel.Content = "Runda gracza " + gracz1_Kopiuj1.Text;
-                    }
-                
-                
             }
             else
             {
                 MessageBox.Show("Wybierz najpierw rozmiar miecza!");
             }
+        }
+
+        private void UstawStanRundy()
+        {
+            if (czyRundaGracza1)
+            {
+                gracz1_przycisk1.IsEnabled = pionkiMalyGracz1 > 0;
+                gracz1_przycisk2.IsEnabled = pionkiSredniGracz1 > 0;
+                gracz1_przycisk3.IsEnabled = pionkiDuzyGracz1 > 0;
+
+                gracz2_przycisk1.IsEnabled = false;
+                gracz2_przycisk2.IsEnabled = false;
+                gracz2_przycisk3.IsEnabled = false;
+
+                rundaLabel.Content = "Runda gracza " + gracz1.Text;
+            }
+            else
+            {
+                gracz1_przycisk1.IsEnabled = false;
+                gracz1_przycisk2.IsEnabled = false;
+                gracz1_przycisk3.IsEnabled = false;
+
+                gracz2_przycisk1.IsEnabled = pionkiMalyGracz2 > 0;
+                gracz2_przycisk2.IsEnabled = pionkiSredniGracz2 > 0;
+                gracz2_przycisk3.IsEnabled = pionkiDuzyGracz2 > 0;
+
+                rundaLabel.Content = "Runda gracza " + gracz1_Kopiuj1.Text;
+            }
+        }
+        private void gracz1_przycisk1_Click(object sender, RoutedEventArgs e)
+        {
+            PostawPionek("Maly", 1);
+        }
+
+   
+        private void gracz1_przycisk2_Click(object sender, RoutedEventArgs e)
+        {
+            PostawPionek("Sredni", 1);
+        }
+
+
+        private void gracz1_przycisk3_Click(object sender, RoutedEventArgs e)
+        {
+            PostawPionek("Duzy", 1);
+        }
+
+
+        private void gracz2_przycisk1_Click(object sender, RoutedEventArgs e)
+        {
+            PostawPionek("Maly", 2);
+        }
+
+ 
+        private void gracz2_przycisk2_Click(object sender, RoutedEventArgs e)
+        {
+            PostawPionek("Sredni", 2);
+        }
+
+       
+        private void gracz2_przycisk3_Click(object sender, RoutedEventArgs e)
+        {
+            PostawPionek("Duzy", 2);
         }
 
     }
